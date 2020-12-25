73ce4cc56cf75b145ce1a93388bd3505f6c3a4e8
fullmetalfenix <jpihlk@yahoo.com>
2020-12-25 09:49:53 -0500

    added image in cache

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src="demo.webp" className="" alt="The child" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
